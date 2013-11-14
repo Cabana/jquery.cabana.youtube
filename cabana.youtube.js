fbe83196b13649488d52eea9f672e8132ced8824
David Pedersen <david.pdrsn@gmail.com>
2013-11-14 11:18:44 +0100

    refactor how data params are applied to options

--- cabana.youtube.js
+++ cabana.youtube.js
@@ -50,27 +50,7 @@
                 return;
             }
 
-            // bind data- params
-            this.options.autohide = ($.type(this.element.data('autohide')) === 'undefined') ? this.options.autohide : this.element.data('autohide');
-            this.options.autoplay = ($.type(this.element.data('autoplay')) === 'undefined') ? this.options.autoplay : this.element.data('autoplay');
-            this.options.cc_load_policy = ($.type(this.element.data('cc-load-policy')) === 'undefined') ? this.options.cc_load_policy : this.element.data('cc-load-policy');
-            this.options.color = ($.type(this.element.data('color')) === 'undefined') ? this.options.color : this.element.data('color');
-            this.options.controls = ($.type(this.element.data('controls')) === 'undefined') ? this.options.controls : this.element.data('controls');
-            this.options.disablekb = ($.type(this.element.data('disablekb')) === 'undefined') ? this.options.disablekb : this.element.data('disablekb');
-            this.options.enablejsapi = ($.type(this.element.data('enablejsapi')) === 'undefined') ? this.options.enablejsapi : this.element.data('enablejsapi');
-            this.options.fs = ($.type(this.element.data('fs')) === 'undefined') ? this.options.fs : this.element.data('fs');
-            this.options.iv_load_policy = ($.type(this.element.data('iv-load-policy')) === 'undefined') ? this.options.iv_load_policy : this.element.data('iv-load-policy');
-            this.options.list = ($.type(this.element.data('list')) === 'undefined') ? this.options.list : this.element.data('list');
-            this.options.listType = ($.type(this.element.data('listtype')) === 'undefined') ? this.options.listType : this.element.data('listtype');
-            this.options.loop = ($.type(this.element.data('loop')) === 'undefined') ? this.options.loop : this.element.data('loop');
-            this.options.modestbranding = ($.type(this.element.data('modestbranding')) === 'undefined') ? this.options.modestbranding : this.element.data('modestbranding');
-            this.options.origin = ($.type(this.element.data('origin')) === 'undefined') ? this.options.origin : this.element.data('origin');
-            this.options.playerapiid = ($.type(this.element.data('playerapiid')) === 'undefined') ? this.options.playerapiid : this.element.data('playerapiid');
-            this.options.playlist = ($.type(this.element.data('playlist')) === 'undefined') ? this.options.playlist : this.element.data('playlist');
-            this.options.rel = ($.type(this.element.data('rel')) === 'undefined') ? this.options.rel : this.element.data('rel');
-            this.options.showinfo = ($.type(this.element.data('showinfo')) === 'undefined') ? this.options.showinfo : this.element.data('showinfo');
-            this.options.theme = ($.type(this.element.data('theme')) === 'undefined') ? this.options.theme : this.element.data('theme');
-            this.options.wmode = ($.type(this.element.data('wmode')) === 'undefined') ? this.options.wmode : this.element.data('wmode');
+            this._applyDataParams();
 
             this._id = this.element.data('id');
             this._container = this.element.wrap('<div class="flex-video"/>');
@@ -78,6 +58,49 @@
 
             this._render();
 
+        },
+
+        /*
+        *   Apply options set through data params
+        */
+        _applyDataParams: function() {
+          var params = [
+            'autohide',
+            'autoplay',
+            'cc_load_policy',
+            'color',
+            'controls',
+            'disablekb',
+            'enablejsapi',
+            'fs',
+            'iv_load_policy',
+            'list',
+            'listType',
+            'loop',
+            'modestbranding',
+            'origin',
+            'playerapiid',
+            'playlist',
+            'rel',
+            'showinfo',
+            'theme',
+            'wmode'
+          ];
+
+          for (var i=0; i < params.length; i++) {
+            var param = params[i];
+
+            this._applyDataParam(param, param);
+          };
+        },
+
+        /*
+        *   Helper method for applying an option set through a data param
+        */
+        _applyDataParam: function(optionToSet, dataParam) {
+            if ($(this.element).data(dataParam)) {
+                this.options[optionToSet] = $(this.element).data(dataParam);
+            }
         },
 
         /*
